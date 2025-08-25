6b2427fadbe28eef9531008e1ad8bf5438a5a221
vedikaJ2012 <vedikamjadhav2004@gmail.com>
2025-08-25 19:18:35 +0530

    Updated Files

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,76 +1,45 @@
-import project1 from "../assets/projects/project-1.jpg";
-import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
+import project1 from "../assets/real_estate.jpg";
+import project2 from "../assets/promanage.jpg";
+import project3 from "../assets/cleanNeighbour.jpg";
+import project4 from "../assets/portfolio.jpg";
 
 export const HomePage_Content = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologies like React, as well as back-end technologies like Node.js, MySQL, PostgreSQL. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
-
-export const EXPERIENCES = [
-  {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-  },
-  {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
-];
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. I have worked with a variety of technologies, including React, Node.js, MySQL, and PostgreSQL. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, and exploring new technologies.`;
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Real Estate Listings",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "Developed a real estate web app similar to the GruhKhoj platform, focused specifically on properties in Kolhapur. The platform allows users to browse, search, and filter property listings with details like price, location, and type.",
+    technologies: ['React', 'Node.js', 'MongoDB', 'TypeScript', 'Tailwind CSS' ],
   },
   {
-    title: "Task Management App",
+    title: "Pro Manage",
     image: project2,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "Built a collaborative task management platform to help teams organize, track, and prioritize work efficiently. The app supports real-time updates, task classification (Backlog, To-Do, In Progress, Done), user authentication, and performance analytics. Pro Manage helps teams to stay aligned, manage workload, and complete projects on time.",
+    technologies: ['React', 'Node.js', 'MongoDB', 'TypeScript', 'Tailwind CSS'],
+  },
+  {
+    title: "CleanNeighbour",
+    image: project3,
+    description:
+      "Developed CleanNeighbour, a web app to help residents report and track improper waste disposal in their communities. The platform allows users to submit complaints with photos, offers anonymous reporting, and includes a reward and penalty system to encourage responsible behavior. It also shows real-time updates on complaint status and shares tips on proper waste management.",
+    technologies: ['React', 'Firebase', 'HTML', 'CSS', 'JavaScript'],
   },
   {
     title: "Portfolio Website",
-    image: project3,
-    description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
-  },
-  {
-    title: "Blogging Platform",
     image: project4,
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "Tailwind CSS", "React", "Bootstrap"],
   },
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "Geetanagar Kavalapur, Tal- Miraj, Dist- Sangli ",
+  phoneNo: "+91 8459420417 ",
+  email: "vedikamjadhav2004@gmail.com",
 };
