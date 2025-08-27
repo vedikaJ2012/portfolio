e292ff697a76ad47ea8b8111c204151653ce77c7
vedikaJ2012 <vedikamjadhav2004@gmail.com>
2025-08-27 19:21:10 +0530

    Added reference links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,37 @@
 import React from 'react'
 import { FaLinkedin } from 'react-icons/fa';
-import { FaGit } from 'react-icons/fa';
-import { FaInstagram } from 'react-icons/fa6';
+import { FaGithub } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
-    <nav className='mb-20 flex items-center justify-between py-6'>
+    <nav className='mb-20 flex items-center justify-between py-6 z-50 relative'>
       <div className='ml-10 text-2xl'>VJ</div>
       <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-        <FaLinkedin />
-        <FaGit />
-        <FaInstagram />
+        <a 
+          href='https://www.linkedin.com/in/vedika-jadhav-57681428b' 
+          target='_blank' 
+          rel='noopener noreferrer'
+          className="hover:text-gray-300 transition-colors"
+        >
+          <FaLinkedin />
+        </a>
+        <a 
+          href='https://github.com/vedikaJ2012' 
+          target='_blank' 
+          rel='noopener noreferrer'
+          className="hover:text-gray-300 transition-colors"
+        >
+          <FaGithub />
+        </a>
+        <a 
+          href='https://www.instagram.com/vedika__jadhav__20' 
+          target='_blank' 
+          rel='noopener noreferrer'
+          className="hover:text-gray-300 transition-colors"
+        >
+          <FaInstagram />
+        </a>
       </div>
     </nav>
   )
